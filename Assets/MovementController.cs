338b132ccda17b67570f8cbf7aefddd248757019
Bastian Hodapp <bastianhodapp@gmail.com>
2019-04-10 15:41:19 +0200

    Added movement controller script

--- Assets/MovementController.cs
+++ Assets/MovementController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //1
+    public float movementSpeed = 3.0f;
+    // 2
+    Vector2 movement = new Vector2();
+    // 3
+    Rigidbody2D rb2D;
+    private void Start()
     {
-        
+        // 4
+        rb2D = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Keep this empty for now
+    }
+    // 5
+    void FixedUpdate()
+    {
+        // 6
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        // 7
+        movement.Normalize();
+        // 8
+        rb2D.velocity = movement * movementSpeed;
     }
 }
