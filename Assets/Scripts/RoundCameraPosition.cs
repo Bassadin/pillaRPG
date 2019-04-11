8f2e3bb520c25b254db21e1d2c44364ddee9ad41
Bastian Hodapp <bastianhodapp@gmail.com>
2019-04-11 11:15:20 +0200

    Added camera rounding script

---
+++ Assets/Scripts/RoundCameraPosition.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Cinemachine;
+public class RoundCameraPos : CinemachineExtension
+{
+    public float pixelsPerUnit = 32;
+    protected override void PostPipelineStageCallback(
+        CinemachineVirtualCameraBase vcam,
+        CinemachineCore.Stage stage, ref CameraState state,
+        float deltaTime)
+    {
+        if (stage == CinemachineCore.Stage.Body)
+        {
+            Vector3 pos = state.FinalPosition;
+            Vector3 pos2 = new Vector3(round(pos.x), round(pos.y), pos.z);
+            state.PositionCorrection += pos2 - pos;
+        }
+    }
+    float round(float x)
+    {
+        return Mathf.Round(x * pixelsPerUnit) / pixelsPerUnit;
+    }
+}
