9c31a4361753caffe399ca7c8b9421baf7a0d80c
Bastian Hodapp <bastianhodapp@gmail.com>
2019-04-24 13:45:03 +0200

    Started GameManager

---
+++ Assets/Scripts/Managers/RPGGameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RPGGameManager : MonoBehaviour
+{
+    public static RPGGameManager sharedInstance = null;
+
+    private void Awake()
+    {
+        if (sharedInstance != null && sharedInstance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            sharedInstance = this;
+        }
+    }
+
+    private void Start()
+    {
+        setupScene();
+    }
+
+    public void setupScene()
+    {
+        //TODO
+    }
+}
