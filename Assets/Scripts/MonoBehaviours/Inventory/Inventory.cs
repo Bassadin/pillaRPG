197b127d1ab90113f1d57297ba7fb7ec03dfbdbb
Bastian Hodapp <bastianhodapp@gmail.com>
2019-04-24 11:14:00 +0200

    Continued inventory script
    Book page 228

--- Assets/Scripts/MonoBehaviours/Inventory/Inventory.cs
+++ Assets/Scripts/MonoBehaviours/Inventory/Inventory.cs
@@ -11,6 +11,53 @@ public class Inventory : MonoBehaviour
 
     public void Start()
     {
-        //Empty for now
+        createSlots();
+    }
+
+    public void createSlots()
+    {
+        if (slotPrefab != null)
+        {
+            for (int i = 0; i < numSlots; i++)
+            {
+                GameObject newSlot = Instantiate(slotPrefab);
+                newSlot.name = "ItemSlot_" + i;
+
+                newSlot.transform.SetParent(gameObject.transform.GetChild(0).transform);
+
+                slots[i] = newSlot;
+                itemImages[i] = newSlot.transform.GetChild(1).GetComponent<Image>();
+            }
+        }
+    }
+
+    public bool addItem(Item itemToAdd)
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (items[i] != null && items[i].itemType == itemToAdd.itemType && itemToAdd.stackable == true)
+            {
+                //Adding to existing slot
+                items[i].quantity++;
+                Slot slotScript = slots[i].gameObject.GetComponent<Slot>();
+                Text quantityText = slotScript.quantityText;
+                quantityText.enabled = true;
+                quantityText.text = items[i].quantity.ToString();
+
+                return true;
+            }
+            else if (items[i] == null)
+            {
+                //Adding empty slot
+                //Copy item & add to inventory. Copying so that the original Scriptable Object isn't changed.
+
+                items[i] = Instantiate(itemToAdd);
+                items[i].quantity = 1;
+                itemImages[i].sprite = itemToAdd.sprite;
+                itemImages[i].enabled = true;
+                return true;
+            }
+        }
+        return false;
     }
 }
