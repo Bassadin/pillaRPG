1433cbf0fd5f40d6a190905057b1c6bfd51de31a
Bastian Hodapp <bastianhodapp@gmail.com>
2019-04-24 10:52:51 +0200

    Started inventory script
    Book page 220

---
+++ Assets/Scripts/MonoBehaviours/Inventory/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject slotPrefab;
+    public const int numSlots = 5;
+    Image[] itemImages = new Image[numSlots];
+    Item[] items = new Item[numSlots];
+    GameObject[] slots = new GameObject[numSlots];
+
+    public void Start()
+    {
+        //Empty for now
+    }
+}
