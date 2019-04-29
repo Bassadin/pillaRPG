dd37f7df2eaa24eb560543a8a9da39c2c8553568
Bastian Hodapp <bastianhodapp@gmail.com>
2019-04-29 13:25:30 +0200

    Added more collision logic

--- Assets/Scripts/MonoBehaviours/Enemy.cs
+++ Assets/Scripts/MonoBehaviours/Enemy.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 public class Enemy : Character
 {
     float hitPoints;
+    public int damageStrength;
+    Coroutine damageCoroutine;
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Player player = collision.gameObject.GetComponent<Player>();
+        }
+    }
     public override IEnumerator damageCharacter(int damage, float interval)
     {
         while(true)
